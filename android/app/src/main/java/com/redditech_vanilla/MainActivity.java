0b0759e2fefbb30ac26a6c7f3799cb959436b3c4
Pierre Perrin <pierre.perrin@epitech.eu>
2021-11-01 11:37:55 +0100

    [FIX] android folder

---
+++ android/app/src/main/java/com/redditech_vanilla/MainActivity.java
@@ -0,0 +1,15 @@
+package com.redditech_vanilla;
+
+import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ReactActivity {
+  @Override
+  protected String getMainComponentName() {
+    return "Redditech_vanilla";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) { 
+    super.onCreate(null);
+  }
+}
